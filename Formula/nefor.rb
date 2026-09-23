4d920f3061bf60826b72c596424979177b5f26b1
amenocturne <git@amenocturne.space>
2026-09-23 18:30:08 +0300

    Remove unintended nefor releases

--- Formula/nefor.rb
+++ Formula/nefor.rb
@@ -1,7 +1,7 @@
 class Nefor < Formula
   desc "Agentic runtime with Lua-composed tools, providers, and workflows"
   homepage "https://github.com/amenocturne/nefor"
-  version "0.7.3"
+  version "0.4.0"
   license "MIT"
 
   # `brew install --HEAD amenocturne/tap/nefor` builds the latest main
@@ -14,21 +14,21 @@ class Nefor < Formula
   on_macos do
     on_arm do
       # slot: MACOS_ARM64
-      url "https://github.com/amenocturne/nefor/releases/download/v0.7.3/nefor-aarch64-apple-darwin.tar.gz"
-      sha256 "c02ef85b3dfa17f4902c7bcf97524cb485253536ff8439764d9dbdaab1dd2c2f"
+      url "https://github.com/amenocturne/nefor/releases/download/v0.4.0/nefor-aarch64-apple-darwin.tar.gz"
+      sha256 "5e6922bead8ed39424c49e510a36a6bb88e90e4eeaeba68143c3b51522dc9cfc"
     end
   end
 
   on_linux do
     on_intel do
       # slot: LINUX_X86_64
-      url "https://github.com/amenocturne/nefor/releases/download/v0.7.3/nefor-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "69fdb697aeba3152abfafad07627ade5221eb78a4528b2e1edc16254743d5b4b"
+      url "https://github.com/amenocturne/nefor/releases/download/v0.4.0/nefor-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "c2bed9404aec3114ec6a80a00a2e9fb32c955d2199c973b73b15b3ff627c3369"
     end
     on_arm do
       # slot: LINUX_ARM64
-      url "https://github.com/amenocturne/nefor/releases/download/v0.7.3/nefor-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0e0b9e7f0ae6da5f3c6c7f53f5e78f7a0601e2dc9733dcf9cefc8765a7b39924"
+      url "https://github.com/amenocturne/nefor/releases/download/v0.4.0/nefor-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "8f16a4b35e551891714c44631f2ee0ec51e57b8c064292a12e713bb7cfd08e95"
     end
   end
 
